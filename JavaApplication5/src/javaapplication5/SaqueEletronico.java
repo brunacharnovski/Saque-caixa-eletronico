24c2f6954fd628005fcf93a8b57827bf395941a4
Bruna Charnovski <brunacharnovski@gmail.com>
2019-09-05 21:52:27 -0300

    Saques de valores inteiros

--- JavaApplication5/src/javaapplication5/SaqueEletronico.java
+++ JavaApplication5/src/javaapplication5/SaqueEletronico.java
@@ -1,14 +1,23 @@
 package javaapplication5;
+
 /*
  * @author Bruna Charnovski
  */
-public class JavaApplication5 {
+public class SaqueEletronico {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int valor = 25;
+        int[] notas = new int[]{2, 5, 10, 20, 50, 100};
+        int[] notasUsadas;
+        notasUsadas = new int[notas.length];
+        int qtNotas = notas.length - 1;
+
+        for (int i = qtNotas; i >= 0; i--) {
+            if (valor % notas[i] == 0) {
+                System.out.println(valor / notas[i] + " nota(s) de " + notas[i]);
+            }
+
+        }
     }
-    
+
 }
